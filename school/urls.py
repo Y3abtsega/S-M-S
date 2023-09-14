db1b9a48c54d5d816f05e9026605c3a1b5f62870
dagm <dadgmg0987@gmial.com>
2023-09-14 12:22:52 -0700

    added url

--- school/urls.py
+++ school/urls.py
@@ -8,5 +8,7 @@ app_name = 'school'
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('login/', views.log, name='login'),
+    path('login/', views.log, name='logggg'),
+
     # Add other URLs as needed
 ]
