33e4e17db5a2374d522a25b08f4be3db002ae8c3
Milki Hirko <140386840+MilkePr@users.noreply.github.com>
2023-10-08 16:24:21 +0300

    Update urls.py
    routed new path for the views created

--- schoolapp/urls.py
+++ schoolapp/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
+
+
+app_name = 'school'
 
 urlpatterns = [
-    path('', views.login_page, name='loginpage'),
-    path('register/', views.register, name='register'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('Grades/', views.Grades, name='Grades'),
-    path('Calender/', views.Calender, name='Calender'),
-    path('Chat/', views.Chat, name='Chat'),
+    path('login/', views.log, name='login'),
+    path('grades/', views.grades, name='grades'),
+    path('chat/', views.chat, name='chat'),
+    path('calender/', views.calender, name='calender'),
+    path('add_grade/', views.add_grade, name='add_grade'),
+    path('login_teacher/', views.login_teacher, name='login_teacher')
+
+    # Add other URLs as needed
 ]
