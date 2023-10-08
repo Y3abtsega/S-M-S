1a830a5cd8c057da4f914bd48a10c8ef1ebecaf8
Milki Hirko <140386840+MilkePr@users.noreply.github.com>
2023-10-08 16:16:40 +0300

    Update views.py
    restructuring views.py

--- schoolapp/views.py
+++ schoolapp/views.py
@@ -1,48 +1,5 @@
-from django.shortcuts import render, redirect, HttpResponse
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
-from django.contrib import messages
+from django.shortcuts import render,redirect
+from django.urls import reverse
 
-
-# Create your views here.
-def register(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        if username == '' or password == '':
-            return redirect('register')
-        user = User.objects.create_user(username=username, password=password)
-        user.save()
-        return HttpResponse("Successfully registered")
-    return render(request, 'register.html')
-
-
-
-def login_page(request) :
-
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        
-        user = authenticate(request, username = username, password = password)
-        if user is not None:
-            login(request, user)
-            return redirect('dashboard')
-        else:
-            messages.add_message(request, messages.WARNING, 'This is a warning message.')
-
-    return render(request, 'login_page.html')
-
-
-def dashboard(request) :
-    return render(request, 'dashboard.html')
-
-def Grades(request) :
-    return render(request, 'Grades.html')
-
-
-def Calender(request) :
-    return render(request, 'Calender.html')
-
-def Chat(request) :
-    return render(request, 'Chat.html')
+def dashboard(request):
+    return render(request, 'school/dashboard.html')
