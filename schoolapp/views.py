1c501caee66feff9b799bc4dfbaa4edb690a3c58
Milki Hirko <140386840+MilkePr@users.noreply.github.com>
2023-10-08 16:20:43 +0300

    Update views.py
    added new views

--- schoolapp/views.py
+++ schoolapp/views.py
@@ -19,3 +19,32 @@ def log(request):
         else:
             return render(request, 'school/login.html', {'error': 'Invalid credentials'})
     return render(request, 'school/login.html')
+
+def add_grade(request):
+
+    if request.method == 'POST':
+
+        subject = request.POST.get('subject')
+        student_name = request.POST.get('student_name')
+        file = request.FILES.get('file')
+        
+        # Create and save a new AddGrade instance in the database
+        AddGrade.objects.create(subject=subject, student_name=student_name, file=file)
+        
+        return redirect('school:add_grade')
+
+    return render(request, 'school/add_grade.html')
+
+def login_teacher(request):
+    if request.method == 'POST':
+
+        user_name = request.POST.get('user_name', '')
+        password = request.POST.get('password', '')
+        user = Teacher.objects.get(user_name=user_name)
+        if user.password==password:
+            return redirect(reverse('school:add_grade'))
+        else:
+            return render(request, 'school/login_teacher.html', {'error': 'Invalid credentials'})
+
+    return render(request, 'school/login_teacher.html')
+    
